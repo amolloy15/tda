4cdb3b1916dfe32292158b07ac9474bd17f266f7
Adam Molloy <amolloy.15@gmail.com>
2022-09-22 19:26:01 -0600

    Added status update functionality

--- utils/calendar.py
+++ utils/calendar.py
@@ -1,5 +1,7 @@
 import datetime
 
+import tda
+
 from enum import Enum
 
 
@@ -18,5 +20,61 @@ class TradeRelativeTime:
 
 
 class Calendar:
-    def __init__(self, td):
+    def __init__(self, td: tda):
         self.startDate = datetime.date.today()
+
+        self._currentTime = datetime.datetime.now()
+
+        self._td = td
+
+        self._marketDay = TradeRelativeDay.Status.nonTradingDay
+        self._marketTime = TradeRelativeTime.Status.marketClosedToday
+
+        self.getStatus()
+
+    def action(self, func):
+        def wrapper():
+            self._currentTime = datetime.datetime.now()
+            func()
+        return wrapper
+
+    def getMarketDay(self):
+        return self._marketDay
+
+    def setMarketDay(self, status: TradeRelativeDay.Status):
+        self._marketDay = status
+
+        if status == TradeRelativeDay.Status.nonTradingDay:
+            self._marketTime = TradeRelativeTime.Status.marketClosedToday
+
+    def getMarketTime(self):
+        return self._marketTime
+
+    def setMarketTime(self, status: TradeRelativeTime.Status):
+        self._marketTime = status
+
+        if status == TradeRelativeTime.Status.marketClosedToday:
+            self._marketDay = TradeRelativeDay.Status.nonTradingDay
+        else:
+            self._marketDay = TradeRelativeDay.Status.tradingDay
+
+    @action
+    def getStatus(self):
+        mkt = tda.client.Client.Markets.EQUITY
+
+        resp = self._td.client.get_hours_for_single_market(mkt, self._currentTime)
+
+        assert resp.status_code == 200
+
+        hours = resp.json()['equity']
+        equityKeys = hours.keys()
+
+        if 'EQ' in equityKeys:
+            equityKey = 'EQ'
+        else:
+            equityKey = 'equity'
+
+        hours = hours[equityKey]['sessionHours']
+
+        if hours is None:
+            self.setMarketTime(TradeRelativeTime.Status.marketClosedToday)
