eff01010dfafdccc8f7d806422b30f5a03863cfd
Adam Molloy <amolloy.15@gmail.com>
2025-05-02 14:14:42 -0500

    Revamp to use heapq

--- utils/scheduler.py
+++ utils/scheduler.py
@@ -1,27 +1,56 @@
 import datetime
-import queue
-import threading
+import dateutil.tz
+import heapq
+import time
 
 from calendar import Calendar
-from schedule import Schedule
+
+from typing import Callable, Any, Dict, List, Optional
 
 
 class Scheduler:
     def __init__(self, calender: Calendar):
-        self.calender = calender
+        self._calender = calender
 
-        self.queue = {}
+        self._tz = dateutil.tz.gettz('America/Chicago')
 
-    def addSchedule(self, schedule: Schedule, func, priority: int):
-        eventTime = schedule.getNextEvent()
+        self._jobs: List[Any] = []
+        self._queue: List[tuple[datetime.datetime, Dict[str, Any]]] = []
 
-        if eventTime in self.queue:
-            eventTime = eventTime - (priority * datetime.timedelta(microseconds=1))
+    def addEvent(self,
+                 eventTime: datetime.datetime,
+                 func: Callable,
+                 args: Optional[List[Any]] = None,
+                 kwargs: Optional[Dict[str, Any]] = None):
+        heapq.heappush(self._queue, (eventTime, {
+            'func': func,
+            'args': args or [],
+            'kwargs': kwargs or {}
+        }))
 
-        self.queue[schedule.getNextEvent()] = func
+    def addJob(self, job: Any):
+        self._jobs.append(job)
 
     def update(self):
-        now = datetime.datetime.now()
-        for eventTime, event in self.queue:
-            if eventTime <= now:
-                event()
+        now = datetime.datetime.now(self._tz)
+        while self._queue and self._queue[0][0] <= now:
+            _, event = heapq.heappop(self._queue)
+            try:
+                event['func'](*event['args'], **event['kwargs'])
+            except Exception as e:
+                print(f"error running {event['func'].__name__: {e}}")
+
+        for job in self._jobs:
+            job.getNextEvent(self)
+
+    def run(self):
+        while True:
+            self.update()
+            now = datetime.datetime.now(self._tz)
+            if self._queue:
+                next_time = self._queue[0][0]
+                sleep_time = max(0, (next_time - now).seconds)
+            else:
+                sleep_time = 1.0
+
+            time.sleep(sleep_time)
